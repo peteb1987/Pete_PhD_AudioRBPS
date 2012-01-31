83ba99cc4af3f140ae5ea4c6ea6cec0b5c862494
Pete <pete.bunch@gmail.com>
2012-01-31 14:24:19 +0000

    Before major rewrite

--- run_audio_smoother.m
+++ run_audio_smoother.m
@@ -17,7 +17,7 @@ params.fs = 44100;                      % Audio sampling frequency
 params.noise_vr = 0.0002;               % Noise variance
 params.ARO = 1;                         % Order of AR model
 params.Np = 20;                         % Number of filtering particles
-params.Ns = 1;                         % Number of smoothing trajectories
+params.Ns = 10;                         % Number of smoothing trajectories
 params.block_length = 1E10;              % Length of processing blocks
 params.init_ref_vr = 0.5;              % Prior variance for reflection coefficients
 params.init_logprocvar_mn = -5;       % Prior mean for log of process variance
@@ -25,7 +25,7 @@ params.init_logprocvar_vr = 0.1;       % Prior variance for log of process varia
 params.procvar_decay = 0.995;          % Decay coefficient on log of process variance
 params.logprocvar_vr = 5E-3;            % Transition variance of log of process variance
 params.ref_trans_vr = 5E-3;            % Transition variance of reflection coefficients
-params.K = 2*params.fs;
+params.K = ceil(0.05*params.fs);
 
 % Get an audio wav file
 [true_audio] = wavread(filename);
@@ -36,10 +36,11 @@ B = fir1(100, 0.25);
 true_audio = filter(B, 1, true_audio);
 true_audio = true_audio(1:ds:end);
 params.fs = params.fs/ds;
-params.K = params.K/ds;
+params.K = ceil(params.K/ds);
 
 % Truncate
-true_audio = true_audio(1:params.K);
+offset = round(params.fs/2);
+true_audio = true_audio(offset+1:offset+params.K);
 
 %%
 
@@ -64,18 +65,18 @@ input_SNR = SNR(true_audio, noisy_audio);
 % output_SNR = SNR(true_audio, est_audio);
 
 %%
-output_SNR = SNR(true_audio(1:22050), est_audio);
-input_SNR = SNR(true_audio(1:22050), noisy_audio(1:22050));
+output_SNR = SNR(true_audio, est_audio);
+input_SNR = SNR(true_audio, noisy_audio);
 
 %%
-figure, hold on, plot(true_audio(1:22000), 'b'), plot(noisy_audio(1:22000), 'r'), plot(est_audio, 'g')
-nonlin_pts = mean(cat(3, pts.nonlin_samp), 3);
-figure, plot(log(nonlin_pts(end,:))');
+% figure, hold on, plot(true_audio, 'b'), plot(noisy_audio, 'r'), plot(est_audio, 'g')
+% nonlin_pts = mean(cat(3, pts.nonlin_samp), 3);
+% figure, plot(log(nonlin_pts(end,:))');
 
 %%
-est_player = audioplayer(est_audio, params.fs);
-noisy_player = audioplayer(noisy_audio, params.fs);
+% est_player = audioplayer(est_audio, params.fs);
+% noisy_player = audioplayer(noisy_audio, params.fs);
 %%
-play(noisy_player)
+% play(noisy_player)
 %%
-play(est_player)
+% play(est_player)
